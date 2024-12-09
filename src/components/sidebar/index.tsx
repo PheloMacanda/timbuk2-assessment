14c787900c73f9841a1db9dfeea8c87acc370b91
Phelo Macanda <42580766+PheloMacanda@users.noreply.github.com>
2024-12-09 15:08:23 +0200

    sidebar and cards (wip)

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,7 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { FaSearch, FaHome, FaBook, FaShoppingCart, FaChartLine, FaCog, FaLifeRing, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import Image2 from '../../assets/image2.png';
 import './sidebar.css';
 
 type Props = {
@@ -7,23 +9,73 @@ type Props = {
 }
 
 export const SideNavBar: FC<Props> = () => {
+
+    const [collapsed, setCollapsed] = useState(false);
+
+    const handleToggle = () => {
+        setCollapsed(!collapsed);
+    };
+
     return (
-        <Sidebar>
-            <Menu>
-                <MenuItem>AG AGoodCompany</MenuItem>
-                <MenuItem>Search Bar</MenuItem>
+        <Sidebar collapsed={collapsed} className='side' width={'35vh'}>
+            {!collapsed && (
+                <div className="profile">
+                    <div className="names">
+                        <div className="avatar">AG</div>
+                        <div className="company-name">AGoodCompany</div>
+                    </div>
+                    <div className="toggle-btn" onClick={handleToggle}>
+                        <FaChevronLeft />
+                    </div>
+                </div>
+            )}
+            {collapsed &&
+                <div className="toggle-btn-collapsed" onClick={handleToggle}>
+                    <FaChevronRight />
+                </div>
+            }
+            {collapsed &&
+                <div className="search-collapsed">
+                    <div className="search-collapsed-container">
+                        <FaSearch />
+                    </div>
+                </div>
+            }
 
-                <MenuItem>Home</MenuItem>
-                <MenuItem>Menu 1</MenuItem>
-                <MenuItem>Menu 2</MenuItem>
-                <MenuItem>Menu 3</MenuItem>
-                <MenuItem>Space....Items</MenuItem>
+            {!collapsed && (
+                <div className="search">
+                    <input type="text" placeholder="Search" />
+                    <div className="icon">
+                        <FaSearch color='gray' />
+                    </div>
+                </div>
+            )}
 
-                <MenuItem>Settings</MenuItem>
-                <MenuItem>Support</MenuItem>
-
-                <MenuItem>Last item</MenuItem>
+            <Menu className='menu1'>
+                <MenuItem icon={<FaHome />}>Home</MenuItem>
+                <MenuItem icon={<FaBook />}>Menu 1</MenuItem>
+                <MenuItem icon={<FaShoppingCart />}>Menu 2</MenuItem>
+                <MenuItem icon={<FaChartLine />}>Menu 3</MenuItem>
             </Menu>
+
+            <Menu className='menu2'>
+                <MenuItem icon={<FaCog />}>Settings</MenuItem>
+                <MenuItem icon={<FaLifeRing />}>Support</MenuItem>
+            </Menu>
+
+            {!collapsed && (
+                <div className='feature-card-container'>
+                    <div className="feature-card">
+                        <div className="feature-text">
+                            <strong>Feature Available now!</strong>
+                            <p>Check out the new dashboard view. Pages now load faster.</p>
+                        </div>
+                        <div className="feature-image">
+                            <img src={Image2} alt="Feature Preview" />
+                        </div>
+                    </div>
+                </div>
+            )}
         </Sidebar>
     );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,19 @@
+import { Card } from '../../components/card';
 import { SideNavBar } from '../../components/sidebar';
 import './home.css';
 
 export const Home = () => {
     return (
-        <>
+        <div className="main-container">
             <SideNavBar />
-        </>
+            <div className="main-app">
+                Your items
+                <div className='cards'>
+                    <Card 
+                        cardText='Item 1'
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
