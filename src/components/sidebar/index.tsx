67fe7165a220b91411046dbba6786a489bd80ec4
Phelo Macanda <42580766+PheloMacanda@users.noreply.github.com>
2024-12-10 12:49:30 +0200

    active menu items

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -8,6 +8,11 @@ import { ReactComponent as Cell } from "../../assets/svg/cell.svg";
 import { ReactComponent as Shop } from "../../assets/svg/shop.svg";
 import { ReactComponent as Graph } from "../../assets/svg/graph.svg";
 import { ReactComponent as Agent } from "../../assets/svg/agent.svg";
+import { ReactComponent as HomeActive } from "../../assets/svg/active/home-active.svg";
+import { ReactComponent as CellActive } from "../../assets/svg/active/cell-active.svg";
+import { ReactComponent as ShopActive } from "../../assets/svg/active/shop-active.svg";
+import { ReactComponent as GraphActive } from "../../assets/svg/active/graph-active.svg";
+import { ReactComponent as AgentActive } from "../../assets/svg/active/agent-active.svg";
 import './sidebar.css';
 
 type Props = {
@@ -15,6 +20,12 @@ type Props = {
 }
 
 export const SideNavBar: FC<Props> = () => {
+
+    const [activeMenu, setActiveMenu] = useState("Home");
+
+    const handleMenuClick = (menu: string) => {
+        setActiveMenu(menu);
+    };
 
     const [collapsed, setCollapsed] = useState(false);
 
@@ -58,15 +69,57 @@ export const SideNavBar: FC<Props> = () => {
             )}
 
             <Menu className='menu1'>
-                <MenuItem icon={<Home />} className="bar-menu-item">Home</MenuItem>
-                <MenuItem icon={<Cell />} className="bar-menu-item">Menu 1</MenuItem>
-                <MenuItem icon={<Shop />} className="bar-menu-item">Menu 2</MenuItem>
-                <MenuItem icon={<Graph />} className="bar-menu-item">Menu 3</MenuItem>
+                <MenuItem
+                    icon={activeMenu === "Home" ? <HomeActive /> : <Home />}
+                    className="bar-menu-item"
+                    active={activeMenu === "Home"}
+                    onClick={() => handleMenuClick("Home")}
+                >
+                    Home
+                </MenuItem>
+                <MenuItem
+                    icon={activeMenu === "Menu 1" ? <CellActive /> : <Cell />}
+                    className="bar-menu-item"
+                    active={activeMenu === "Menu 1"}
+                    onClick={() => handleMenuClick("Menu 1")}
+                >
+                    Menu 1
+                </MenuItem>
+                <MenuItem
+                    icon={activeMenu === "Menu 2" ? <ShopActive /> : <Shop />}
+                    className="bar-menu-item"
+                    active={activeMenu === "Menu 2"}
+                    onClick={() => handleMenuClick("Menu 2")}
+                >
+                    Menu 2
+                </MenuItem>
+                <MenuItem
+                    icon={activeMenu === "Menu 3" ? <GraphActive /> : <Graph />}
+                    className="bar-menu-item"
+                    active={activeMenu === "Menu 3"}
+                    onClick={() => handleMenuClick("Menu 3")}
+                >
+                    Menu 3
+                </MenuItem>
             </Menu>
 
             <Menu className='menu2'>
-                <MenuItem icon={<MdOutlineSettings size={24} />} className="bar-menu-item">Settings</MenuItem>
-                <MenuItem icon={<Agent />} className="bar-menu-item">Support</MenuItem>
+                <MenuItem 
+                    icon={<MdOutlineSettings size={24} color={activeMenu === "Settings" ? "#6200ea" : "black"} />} 
+                    className="bar-menu-item"
+                    active={activeMenu === "Settings"}
+                    onClick={() => handleMenuClick("Settings")}
+                >
+                    Settings
+                </MenuItem>
+                <MenuItem 
+                    icon={activeMenu === "Support" ? <AgentActive /> :<Agent />} 
+                    className="bar-menu-item"
+                    active={activeMenu === "Support"}
+                    onClick={() => handleMenuClick("Support")}
+                >
+                    Support
+                </MenuItem>
             </Menu>
 
             {!collapsed && (
